03f1ba3ec35887d360014fd0d7d38a4f7532d1bc
Douglas-Castro <farias.douglascastro@gmail.com>
2021-03-24 20:49:39 -0300

    :bulb: Add login and logout controller and refactored login controller

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,20 +1,63 @@
-const Login = require('../models/loginModel')
+const User = require('../models/userModel')
 
 exports.index = (req, res) => {
+  if (req.session.user) return res.render('logged-in')
   res.render('login')
 }
 
 exports.register = async function (req, res) {
-  const login = new Login(req.body)
-  await login.register()
+  try {
+    const user = new User(req.body)
+    await user.register()
 
-  if (login.errors.length > 0) {
-    req.flash('errors', login.errors)
+    if (user.errors.length > 0) {
+      req.flash('errors', user.errors)
+      req.session.save(function () {
+        return res.redirect('/login')
+      })
+
+      return
+    }
+
+    req.flash('success', 'Your account was created.')
     req.session.save(function () {
-      res.redirect('/login')
+      return res.redirect('/login')
     })
+  } catch (e) {
+    console.log(e)
 
-    return
+    return res.render('404')
   }
-  res.send(login.errors)
+}
+
+exports.login = async function (req, res) {
+  try {
+    const user = new User(req.body)
+    await user.login()
+
+    if (user.errors.length > 0) {
+      req.flash('errors', user.errors)
+      req.session.save(function () {
+        return res.redirect('/login')
+      })
+
+      return
+    }
+
+    req.flash('success', 'YAY! You are logged in.')
+    req.session.user = user.user
+
+    req.session.save(function () {
+      return res.redirect('/login')
+    })
+  } catch (e) {
+    console.log(e)
+
+    return res.render('404')
+  }
+}
+
+exports.logout = function (req, res) {
+  req.session.destroy()
+  res.redirect('/')
 }
