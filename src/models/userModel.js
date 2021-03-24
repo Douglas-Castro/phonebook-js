e278f8bcf7253d1acbcbe751ee13c16c83d614e3
Douglas-Castro <farias.douglascastro@gmail.com>
2021-03-24 20:44:33 -0300

    :arrow_up: Update user models(file has been renamed)

---
+++ src/models/userModel.js
@@ -0,0 +1,80 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+const bcryptjs = require('bcryptjs')
+
+const UserSchema = new mongoose.Schema({
+  email: { type: String, require: true },
+  password: { type: String, require: true }
+})
+
+const UserModel = mongoose.model('User', UserSchema)
+
+class User {
+  constructor (body) {
+    this.body = body
+    this.errors = []
+    this.user = null
+  }
+
+  async login () {
+    if (this.errors.length > 0) return
+
+    this.user = await UserModel.findOne({ email: this.body.email })
+
+    if (!this.user) {
+      this.errors.push('• User doesn\'t.')
+
+      return
+    }
+
+    if (!bcryptjs.compareSync(this.body.password, this.user.password)) {
+      this.errors.push('• Incorrect password.')
+      this.user = null
+    }
+  }
+
+  async register () {
+    this.validate()
+    if (this.errors.length > 0) return
+
+    await this.userExists()
+
+    if (this.errors.length > 0) return
+
+    const salt = bcryptjs.genSaltSync()
+    this.body.password = bcryptjs.hashSync(this.body.password, salt)
+
+    this.user = await UserModel.create(this.body)
+  }
+
+  async userExists () {
+    this.user = await UserModel.findOne({ email: this.body.email })
+    if (this.user) this.errors.push('• User already exists.')
+  }
+
+  validate () {
+    this.cleanUp()
+
+    // Email Validation
+    if (!validator.isEmail(this.body.email)) this.errors.push('• Please, enter a valid email.')
+
+    // Password Validation
+    if (this.body.password.length < 5) this.errors.push('• Your password must be at least 5 characters.')
+    if (this.body.password.length >= 50) this.errors.push('• Your password must be no longer than 50 characters.')
+  }
+
+  cleanUp () {
+    for (const key in this.body) {
+      if (typeof this.body[key] !== 'string') {
+        this.body[key] = ''
+      }
+    }
+
+    this.body = {
+      email: this.body.email,
+      password: this.body.password
+    }
+  }
+}
+
+module.exports = User
