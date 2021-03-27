f64491e802aa8903bdec1d75103512ac6376566b
Douglas-Castro <farias.douglascastro@gmail.com>
2021-03-27 00:28:43 -0300

    :arrow_up: Update contact function for contact class

--- src/models/contactModel.js
+++ src/models/contactModel.js
@@ -10,47 +10,64 @@ const ContactSchema = new mongoose.Schema({
 
 const ContactModel = mongoose.model('Contact', ContactSchema)
 
-function Contact (body) {
-  this.body = body
-  this.errors = []
-  this.contact = null
-}
+class Contact {
+  constructor (body) {
+    this.body = body
+    this.errors = []
+    this.contact = null
+  }
 
-Contact.searchById = async function (id) {
-  if (typeof id !== 'string') return
-
-  const contact = await ContactModel.findById(id)
-
-  return contact
-}
-
-Contact.prototype.register = async function () {
-  this.validate()
-
-  if (this.errors.length > 0) return
-
-  this.contact = await ContactModel.create(this.body)
-}
-
-Contact.prototype.validate = function () {
-  this.cleanUp()
-
-  if (this.body.email && !validator.isEmail(this.body.email)) this.errors.push('• Invalid Email.')
-  if (!this.body.name) this.errors.push('• Please, contact name needs to be put.')
-  if (!this.body.email && !this.body.phone_number) this.errors.push('• Please, enter at least one contact information: e-mail or phone number.')
-}
-
-Contact.prototype.cleanUp = function () {
-  for (const key in this.body) {
-    if (typeof this.body[key] !== 'string') {
-      this.body[key] = ''
+  async searchById (id) {
+    try {
+      this.contact = await ContactModel.findById(id)
+      return this.contact
+    } catch (e) {
+      console.log(e)
     }
   }
 
-  this.body = {
-    name: this.body.name,
-    email: this.body.email,
-    phone_number: this.body.phone_number
+  async register () {
+    try {
+      this.validate()
+      if (this.errors.length > 0) return
+
+      this.contact = await ContactModel.create(this.body)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  validate () {
+    this.cleanUp()
+
+    if (this.body.email && !validator.isEmail(this.body.email)) this.errors.push('• Invalid Email.')
+    if (!this.body.name) this.errors.push('• Please, contact name needs to be put.')
+    if (!this.body.email && !this.body.phone_number) this.errors.push('• Please, enter at least one contact information: e-mail or phone number.')
+  }
+
+  cleanUp () {
+    for (const key in this.body) {
+      if (typeof this.body[key] !== 'string') {
+        this.body[key] = ''
+      }
+    }
+
+    this.body = {
+      name: this.body.name,
+      email: this.body.email,
+      phone_number: this.body.phone_number
+    }
+  }
+
+  async edit (id) {
+    try {
+      this.validate()
+      if (this.errors.length > 0) return
+
+      this.contact = await ContactModel.findOneAndUpdate(id, this.body, { new: true })
+    } catch (e) {
+      console.log(e)
+    }
   }
 }
 
