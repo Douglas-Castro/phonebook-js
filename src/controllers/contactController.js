93d1a2eb4c1234df900ca4fa07c360a053b75d0a
Douglas-Castro <farias.douglascastro@gmail.com>
2021-03-27 00:26:40 -0300

    :arrow_up: Refactor contact controller

--- src/controllers/contactController.js
+++ src/controllers/contactController.js
@@ -1,37 +1,65 @@
 const Contact = require('../models/contactModel')
 
-exports.index = (req, res) => {
+exports.index = function (req, res) {
   res.render('contact', {
     contact: {}
   })
 }
 
-exports.register = async (req, res) => {
+exports.register = async function (req, res) {
   try {
     const contact = new Contact(req.body)
     await contact.register()
 
     if (contact.errors.length > 0) {
       req.flash('errors', contact.errors)
-      req.session.save(() => res.redirect('/contact'))
 
-      return
+      req.session.save(function () {
+        return res.redirect('/contact')
+      })
     }
 
     req.flash('success', 'The contact has been added to your phone book.')
-    req.session.save(() => res.redirect(`/contact/${contact.contact._id}`))
+    req.session.save(function () {
+      return res.redirect(`/contact/${contact.contact._id}`)
+    })
   } catch (e) {
     console.log(e)
     return res.render('404')
   }
 }
 
-exports.contactEdit = async (req, res) => {
-  if (!req.params.id) return res.render('404')
+exports.contact = async function (req, res) {
+  try {
+    let contact = new Contact()
+    contact = await contact.searchById(req.params.id)
 
-  const contact = await Contact.searchById(req.params.id)
+    res.render('contact', { contact })
+  } catch (e) {
+    console.log(e)
+  }
+}
 
-  if (!contact) return res.render('404')
+exports.edit = async function (req, res) {
+  try {
+    if (!req.params.id) return res.render('404')
 
-  res.render('contact', { contact })
+    let contact = new Contact()
+    contact = await contact.edit(req.params.id)
+
+    console.log('caraaai' + contact)
+
+    if (contact.errors.length > 0) {
+      req.flash('errors', contact.errors)
+      req.session.save(() => res.redirect(`/contact/${contact.contact._id}`))
+
+      return
+    }
+
+    req.flash('success', 'The contact edit has been saved.')
+    req.session.save(() => res.redirect(`/contact/${contact.contact._id}`))
+  } catch (e) {
+    console.log(e)
+    res.render('404')
+  }
 }
