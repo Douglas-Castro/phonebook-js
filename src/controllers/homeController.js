6f92db35560e08a33af595b9f111af6a57c8809e
Douglas-Castro <farias.douglascastro@gmail.com>
2021-03-28 04:34:48 -0300

    :bulb: Add contact

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,3 +1,11 @@
-exports.index = (req, res) => {
-  res.render('index')
+const Contact = require('../models/contactModel')
+
+exports.index = async (req, res) => {
+  try {
+    const contact = new Contact()
+    const contacts = await contact.searchContacts()
+    res.render('index', { contacts })
+  } catch (e) {
+    console.log(e)
+  }
 }
