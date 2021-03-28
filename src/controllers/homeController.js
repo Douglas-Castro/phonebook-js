881f4622f6cd1fa05026525201b8219393ec9d4d
Douglas-Castro <farias.douglascastro@gmail.com>
2021-03-28 04:35:31 -0300

    :bulb: Add display of contacts in the index

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -4,6 +4,7 @@ exports.index = async (req, res) => {
   try {
     const contact = new Contact()
     const contacts = await contact.searchContacts()
+    
     res.render('index', { contacts })
   } catch (e) {
     console.log(e)
